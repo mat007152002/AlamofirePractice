c45716c74c56b59a526e3a4953eed6c24f33c536
Matthew Ling <mat007152002g@gmail.com>
2020-12-29 13:16:18 +0800

    Initial Commit

---
+++ AlamofirePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofirePractice
+//
+//  Created by 旌榮 凌 on 2020/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
